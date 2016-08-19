41d747fa05f5f08ab92ee9562e89725c2b163d2a
FujimotoTakuo <takuo-fujimoto@to-be.co.jp>
2016-08-19 17:05:35 +0900

    dsdad
    da

--- src/main/java/com/example/managedvms/helloworld/MainServlet.java
+++ src/main/java/com/example/managedvms/helloworld/MainServlet.java
@@ -1,1 +1,44 @@
-dd
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.managedvms.helloworld;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.fjmt.app.main.*
+
+// [START example]
+@WebServlet(name = "main", value = "/main")
+@SuppressWarnings("serial")
+public class MainServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    PrintWriter out = resp.getWriter();
+    out.println("Hello, world");
+    try {
+      Main.main();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
+// [END example]
